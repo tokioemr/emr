4545a1d91adfd620d38590ddd8c9e43e7f5f474d
Szymon Uglis <szymon@l7ssha.xyz>
2022-02-06 02:17:15 +0100

    Add validation for empty json fields

--- src/main/kotlin/xyz/l7ssha/emr/dto/security/JwtRequestInputDto.kt
+++ src/main/kotlin/xyz/l7ssha/emr/dto/security/JwtRequestInputDto.kt
@@ -1,11 +1,14 @@
 package xyz.l7ssha.emr.dto.security
 
+import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 class JwtRequestInputDto {
     @NotNull
+    @NotEmpty
     lateinit var username: String
 
     @NotNull
+    @NotEmpty
     lateinit var password: String
 }
